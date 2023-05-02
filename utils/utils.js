7684b473f0a971434f0e773496cbc1b280b654c8
Denis Hazheev <denishazheev@MacBook-Pro-Denis.local>
2023-05-03 02:00:21 +0300

    Испрааление критических ошибок

--- utils/utils.js
+++ utils/utils.js
@@ -2,7 +2,7 @@ const INCORRECT_DATA_ERROR_CODE = 400;
 const NOT_FOUND_ERROR_CODE = 404;
 const DEFAULT_ERROR_CODE = 500;
 const OK_STATUS_CODE = 200;
-const default_error_message = {message:'Something went wrong'};
+const default_error_message = {message:'An error occurred on the server'};
 
 module.exports = {
     INCORRECT_DATA_ERROR_CODE,
